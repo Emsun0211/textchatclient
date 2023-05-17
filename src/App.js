0b7bce58c5a6e559b7d2cebc6a01fe0b65aa6152
Emsun <emsunconcept@gmail.com>
2023-05-17 21:12:11 +0100

    added the server

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./component/Chat";
 
-const socket = io.connect("http://localhost:5000");
+// const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://textchatserver.onrender.com");
 
 function App() {
 	const [username, setusername] = useState("");
